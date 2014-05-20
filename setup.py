ee62e597636c15f5ccffc57f3e306697ebbc846a
Chmouel Boudjnah <chmouel@enovance.com>
2014-05-20 17:26:06 +0200

    Add license fix pep8 to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+#
+# Author: eNovance <dev@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from setuptools import setup
 
 setup(
@@ -13,6 +30,7 @@ setup(
         'Environment :: No Input/Output (Daemon)'],
     install_requires = ['swift', ],
     entry_points = {
-        'paste.app_factory': ['rados_object = swift_ceph_backend.rados_server:app_factory'],
+        'paste.app_factory': [
+            'rados_object = swift_ceph_backend.rados_server:app_factory'],
     },
 )
